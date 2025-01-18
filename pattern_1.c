123cb7ef4f5aa0c769277d5f5918fff970146830
Deepak Singh <158508871+Deepaksingh0510@users.noreply.github.com>
2025-01-18 22:37:15 +0530

    Update pattern_1.c

--- pattern_1.c
+++ pattern_1.c
@@ -30,29 +30,29 @@ int main(){
 //    0 1 2 
 //  0 1 2 3 4 
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
-//     int n;
-//     scanf("%d",&n);
+int main(){
+    int n;
+    scanf("%d",&n);
 
-//     for (int i = 0; i <n; i++)
-//     {
-//         for (int j = 0; j <(2*(n-i)-1); j++)
-//         {
-//             printf(" ");
-//         }
+    for (int i = 0; i <n; i++)
+    {
+        for (int j = 0; j <(2*(n-i)-1); j++)
+        {
+            printf(" ");
+        }
 
-//         for (int k = 0; k < 2*i+1; k++)
-//         {
-//             printf("%d ",k);
-//         }
-//         printf("\n");
+        for (int k = 0; k < 2*i+1; k++)
+        {
+            printf("%d ",k);
+        }
+        printf("\n");
         
         
         
-//     }
+    }
     
-//     return 0;
-// }
+    return 0;
+}
 
